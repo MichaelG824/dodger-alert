3566be3dde73ce2cb403da585ce004f362f9a75f
Michael Gee <mikedodger10@gmail.com>
2017-08-07 07:58:58 +0000

    Added game time data

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ var bodyparser = require('body-parser');
 var session = require('express-session');
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
+var mlb = require('mlb-scores');
+
+//Error handle: 
+//It is behind 3 games.
+mlb.getGameUrl('dodgers', new Date(2017,8,9), function(err, url){
+  if (!err){
+    mlb.getGameInfo(url, function(err, data){
+      if(err)
+          throw err;
+          
+      console.log(JSON.stringify(data, null, 4));
+    });
+  }
+});
 
 //Setup express app.
 var app = express();
@@ -27,3 +41,4 @@ app.use(session({secret:"BiggieNYC", resave:false, saveUninitialized:true}));
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Your are now on port: ", process.env.PORT);
 });
+
