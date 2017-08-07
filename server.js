65028bd76f98f5d34966839c6592dca7ccaf72b3
Michael Gee <mikedodger10@gmail.com>
2017-08-07 06:18:48 +0000

    Basic server stuff

--- server.js
+++ server.js
@@ -1,84 +1,29 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var bodyparser = require('body-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
+//Setup express app.
+var app = express();
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
+//Middleware
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyparser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyparser.json()); // parse application/json
+app.use(function (req, res, next){ //configure for cross origin headers
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+});
 
-    sockets.push(socket);
+//Update requests to console.
+app.use(session({secret:"BiggieNYC", resave:false, saveUninitialized:true}));
 
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+//listen to port
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Your are now on port: ", process.env.PORT);
 });
